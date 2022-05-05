bb5dde0828d1ea9756942b9102c66b1a2f7f2c13
Lucas Feijó <lucas.portela@vtex.com.br>
2022-05-05 15:33:25 -0300

    Applies `gatsby.store`'s patch

--- src/components/ui/Link/Link.tsx
+++ src/components/ui/Link/Link.tsx
@@ -1,22 +1,23 @@
 import { Link as UILink } from '@faststore/ui'
 import type { ElementType } from 'react'
-import type { LinkProps } from '@faststore/ui'
+import type { LinkProps as UILinkProps } from '@faststore/ui'
 
 import FrameworkLink from 'src/components/common/Link'
 
-type Variant = 'default' | 'display' | 'inline' | 'footer'
+type Variant = 'default' | 'display' | 'footer' | 'inline'
 
-type Props<T extends ElementType = typeof FrameworkLink> = LinkProps<T> & {
-  variant?: Variant
-  inverse?: boolean
-}
+export type LinkProps<T extends ElementType = typeof FrameworkLink> =
+  UILinkProps<T> & {
+    variant?: Variant
+    inverse?: boolean
+  }
 
 function Link<T extends ElementType = typeof FrameworkLink>({
   variant = 'default',
   inverse,
   href,
-  ...props
-}: Props<T>) {
+  ...otherProps
+}: LinkProps<T>) {
   return (
     <UILink
       as={FrameworkLink}
@@ -24,7 +25,7 @@ function Link<T extends ElementType = typeof FrameworkLink>({
       data-fs-link-variant={variant}
       data-fs-link-inverse={inverse}
       href={href}
-      {...props}
+      {...otherProps}
     />
   )
 }
