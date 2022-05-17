af7658d733016650a97d536c90820c538f86a1cd
Daniel Zanzini <daniel.zanzini@vtex.com.br>
2022-05-17 11:01:11 -0300

    Uses camel-case classes for css modules

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
     locales: ['en-US'],
     defaultLocale: 'en-US',
   },
+  webpack: (config) => {
+    // camel-case style names from css modules
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes('css-loader'))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = 'camelCase'
+        }
+      })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
