05efba13bd5157f5f8050b4a79376f82461c3e05
Arthur Andrade <arthurfelandrade@gmail.com>
2022-06-07 09:50:07 -0300

    feat: add tokens to RegionalizationBar
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/components/regionalization/RegionalizationBar/RegionalizationBar.tsx
+++ src/components/regionalization/RegionalizationBar/RegionalizationBar.tsx
@@ -5,6 +5,8 @@ import Button from 'src/components/ui/Button'
 import Icon from 'src/components/ui/Icon'
 import { useModal } from 'src/sdk/ui/modal/Provider'
 
+import styles from './regionalization-bar.module.scss'
+
 interface RegionalizationBarProps extends HTMLAttributes<HTMLDivElement> {
   classes: string
 }
@@ -17,16 +19,22 @@ export default function RegionalizationBar({
   const { postalCode } = useSession()
 
   return (
-    <div data-fs-regionalization-bar className={classes} {...otherProps}>
+    <div
+      data-fs-regionalization-bar
+      className={`${classes} ${styles.fsRegionalizationBar}`}
+      {...otherProps}
+    >
       <Button onClick={() => setIsRegionalizationModalOpen(true)}>
         <Icon name="MapPin" width={24} height={24} />
         {postalCode ? (
           <>
-            <span>{postalCode}</span>
-            <span>Edit</span>
+            <span data-fs-regionalization-bar-postal-code>{postalCode}</span>
+            <span data-fs-regionalization-bar-edit-message>Edit</span>
           </>
         ) : (
-          <span>Set your location</span>
+          <span data-fs-regionalization-bar-input-message>
+            Set your location
+          </span>
         )}
         <Icon name="CaretRight" width={24} height={24} />
       </Button>
