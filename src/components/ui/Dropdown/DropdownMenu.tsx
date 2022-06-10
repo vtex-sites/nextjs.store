4961087dd934f5df0383703a0136101f1fbb8e82
Fanny Chien <fannychienn@gmail.com>
2022-06-10 14:22:10 -0300

    Adds small size variant

--- src/components/ui/Dropdown/DropdownMenu.tsx
+++ src/components/ui/Dropdown/DropdownMenu.tsx
@@ -3,14 +3,25 @@ import type { DropdownMenuProps } from '@faststore/ui'
 
 import styles from './dropdown.module.scss'
 
+export type Size = 'small' | 'regular'
+
+export type Props = DropdownMenuProps & {
+  /**
+   * Specifies the size variant
+   */
+  size?: Size
+}
+
 function DropdownMenu({
   children,
+  size = 'regular',
   testId = 'store-dropdown-menu',
   ...otherProps
-}: DropdownMenuProps) {
+}: Props) {
   return (
     <UIDropdownMenu
       data-fs-dropdown-menu
+      data-fs-dropdown-menu-size={size}
       className={styles.fsDropdownMenu}
       data-testid={testId}
       {...otherProps}
