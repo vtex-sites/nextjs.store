09dd43ff9f8ce0884fd7cdafd6b03385991bfa64
Tiago Gimenes <tlgimenes@gmail.com>
2022-06-20 11:44:47 -0300

    fix lint

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+
 function Logo() {
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img src="/logo.svg" alt="The store logo with with a stylized letter B in a dark blue gradient
-        background" width="176px" height="30px" />
+  return (
+    <img
+      src="/logo.svg"
+      alt="The store logo with with a stylized letter B in a dark blue gradient background"
+      width="176px"
+      height="30px"
+    />
+  )
 }
 
 export default Logo
