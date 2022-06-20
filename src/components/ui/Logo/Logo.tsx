b20c84500dd9643f7ef3ee46ae60694a0add0bdd
Tiago Gimenes <tlgimenes@gmail.com>
2022-06-20 11:05:11 -0300

    Update src/components/ui/Logo/Logo.tsx
    Co-authored-by: Filipe W. Lima <fwl.ufpe@gmail.com>

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,6 +1,7 @@
 function Logo() {
   // eslint-disable-next-line @next/next/no-img-element
-  return <img src="/logo.svg" alt="logo" width="176px" height="30px" />
+  return <img src="/logo.svg" alt="The store logo with with a stylized letter B in a dark blue gradient
+        background" width="176px" height="30px" />
 }
 
 export default Logo
