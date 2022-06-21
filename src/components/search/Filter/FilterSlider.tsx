5887dc0c67c17ac4cfddf346ff49bc3fd37e309d
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-06-21 20:18:08 -0300

    Crestes FilterSlider stories

--- src/components/search/Filter/FilterSlider.tsx
+++ src/components/search/Filter/FilterSlider.tsx
@@ -12,6 +12,9 @@ import styles from './filter-slider.module.scss'
 import type { useFilter } from './useFilter'
 
 interface Props {
+  /**
+   * The array that represents the details of every facet.
+   */
   facets: Filter_FacetsFragment[]
   /**
    * ID to find this component in testing tools (e.g.: cypress,
