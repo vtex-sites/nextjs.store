c5501bb75ba9865b8e3b47a5d586eeb504834ad5
renatamottam <renatamotta.m@gmail.com>
2022-06-23 14:59:05 -0300

    Fixes Best Practices section

--- .storybook/components/BestPracticesRule.tsx
+++ .storybook/components/BestPracticesRule.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from 'react'
 
 type BestPracticesRuleProps = {
-  recommendedUsage: ReactNode
-  discouragedUsage: ReactNode
+  recommendedUsage?: ReactNode
+  discouragedUsage?: ReactNode
   recommendedDescription: string
   discouragedDescription: string
 }
@@ -16,7 +16,7 @@ const BestPracticesRule = ({
   return (
     <section className="sbdocs-best-practices-rule">
       <article>
-        <div>{recommendedUsage}</div>
+        {recommendedUsage && <div>{recommendedUsage}</div>}
         <article className="sbdocs-best-practices-text">
           <h3 className="sbdocs sbdocs-h3">
             <span role="img" aria-label="Check Mark">
@@ -28,7 +28,7 @@ const BestPracticesRule = ({
         </article>
       </article>
       <article>
-        <div>{discouragedUsage}</div>
+        {discouragedUsage && <div>{discouragedUsage}</div>}
         <article className="sbdocs-best-practices-text">
           <h3 className="sbdocs sbdocs-h3">
             <span aria-label="Cross Mark">&#10060;</span> Don't
