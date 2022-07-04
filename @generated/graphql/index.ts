c8832077afcb443eace40d04c0d809af09780c55
Victor Miranda <victor2142@gmail.com>
2022-07-04 15:03:51 -0300

    Update generated types from GraphQL queries

--- @generated/graphql/index.ts
+++ @generated/graphql/index.ts
@@ -676,7 +676,23 @@ export type ProductDetailsFragment_ProductFragment = {
   gtin: string
   description: string
   id: string
-  isVariantOf: { productGroupID: string; name: string }
+  isVariantOf: {
+    productGroupID: string
+    name: string
+    hasVariant: Array<{
+      slug: string
+      name: string
+      productID: string
+      seo: { title: string }
+      image: Array<{ url: string; alternateName: string }>
+      additionalProperty: Array<{
+        propertyID: string
+        value: any
+        name: string
+        valueReference: string
+      }>
+    }>
+  }
   image: Array<{ url: string; alternateName: string }>
   brand: { name: string }
   offers: {
@@ -778,7 +794,23 @@ export type ServerProductPageQueryQuery = {
         seller: { identifier: string }
       }>
     }
-    isVariantOf: { productGroupID: string; name: string }
+    isVariantOf: {
+      productGroupID: string
+      name: string
+      hasVariant: Array<{
+        slug: string
+        name: string
+        productID: string
+        seo: { title: string }
+        image: Array<{ url: string; alternateName: string }>
+        additionalProperty: Array<{
+          propertyID: string
+          value: any
+          name: string
+          valueReference: string
+        }>
+      }>
+    }
     additionalProperty: Array<{
       propertyID: string
       name: string
@@ -863,7 +895,23 @@ export type BrowserProductQueryQuery = {
     gtin: string
     description: string
     id: string
-    isVariantOf: { productGroupID: string; name: string }
+    isVariantOf: {
+      productGroupID: string
+      name: string
+      hasVariant: Array<{
+        slug: string
+        name: string
+        productID: string
+        seo: { title: string }
+        image: Array<{ url: string; alternateName: string }>
+        additionalProperty: Array<{
+          propertyID: string
+          value: any
+          name: string
+          valueReference: string
+        }>
+      }>
+    }
     image: Array<{ url: string; alternateName: string }>
     brand: { name: string }
     offers: {
