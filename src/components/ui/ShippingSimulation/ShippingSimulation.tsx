2b9aec25033b5298be645556a8034f90ef11fe78
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-07-27 22:38:06 -0300

    Adds conditional rendering and session checking

--- src/components/ui/ShippingSimulation/ShippingSimulation.tsx
+++ src/components/ui/ShippingSimulation/ShippingSimulation.tsx
@@ -1,7 +1,7 @@
 import { useSession } from '@faststore/sdk'
 import { Table, TableBody, TableCell, TableRow } from '@faststore/ui'
-import type { HTMLAttributes } from 'react'
-import { useRef, useState } from 'react'
+import type { ChangeEvent, HTMLAttributes } from 'react'
+import { useEffect, useState } from 'react'
 
 import Price from 'src/components/ui/Price'
 import { usePriceFormatter } from 'src/sdk/product/useFormattedPrice'
@@ -11,15 +11,26 @@ import InputText from '../InputText'
 import Link from '../Link'
 import styles from './shipping-simulation.module.scss'
 
+type ShippingOptionProps = {
+  carrier: string
+  estimate: string
+  price: number
+}
+
 interface ShippingSimulationProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * ID to find this component in testing tools (e.g.: cypress,
    * testing-library, and jest).
    */
   testId?: string
+  /**
+   * Array of ShippingOptionProps
+   */
+  options?: ShippingOptionProps[]
 }
 
-const shippingOptions = [
+// TODO Remove default values after API integration
+const defaultShippingOptions = [
   {
     carrier: 'Regular',
     estimate: '12 days',
@@ -44,28 +55,72 @@ const shippingOptions = [
 
 function ShippingSimulation({
   testId = 'store-shipping-simulation',
+  options,
   ...otherProps
 }: ShippingSimulationProps) {
-  const postalCodeInputRef = useRef<HTMLInputElement>(null)
-  const { postalCode } = useSession()
-  const [postalCodeInput, setPostalCodeInput] = useState(postalCode ?? '')
-  const [postalCodeLocation, setPostalCodeLocation] = useState(
-    'Mt. Street — Newark, NY'
+  const { postalCode: sessionPostalCode } = useSession()
+
+  const [shippingPostalCode, setShippingPostalCode] = useState('')
+  const [displayClearButton, setDisplayClearButton] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const [shippingOptions, setShippingOptions] = useState<ShippingOptionProps[]>(
+    []
   )
+
+  const [postalCodeLocation, setPostalCodeLocation] = useState('')
+
+  useEffect(() => {
+    if (!sessionPostalCode || shippingPostalCode) return
+
+    // Use session postal code if there is no shippingPostalCode
+    setShippingPostalCode(sessionPostalCode)
+    setDisplayClearButton(true)
+
+    // TODO update after API integration
+    setShippingOptions(options ?? [])
+    setPostalCodeLocation('Mt. Street — Newark, NY')
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [options, sessionPostalCode])
 
   const formatter = usePriceFormatter()
 
   const handleSubmit = () => {
-    const value = postalCodeInputRef.current?.value
+    setDisplayClearButton(true)
 
-    // API integration with shipping simulation resolver
-    setPostalCodeLocation(`Updated ${value} Location`)
+    try {
+      // TODO Change next lines after API integration
+      setPostalCodeLocation(`Updated ${shippingPostalCode} location`)
+      setShippingOptions(defaultShippingOptions)
+    } catch (error) {
+      setErrorMessage('You entered an invalid Zip Code')
+    }
   }
+
+  const handleOnInput = (e: ChangeEvent<HTMLInputElement>) => {
+    if (errorMessage !== '') {
+      setErrorMessage('')
+    }
+
+    const currentValue = e.currentTarget.value
+
+    setShippingPostalCode(currentValue)
+    setDisplayClearButton(false)
+
+    if (!currentValue) {
+      setShippingOptions([])
+      setPostalCodeLocation('')
+    }
+  }
+
+  const hasShippingOptions = !!shippingOptions && shippingOptions.length > 0
 
   return (
     <section
       className={styles.fsShippingSimulation}
       data-fs-shipping-simulation
+      data-fs-shipping-simulation-empty={!hasShippingOptions ? 'true' : 'false'}
       data-testid={testId}
       {...otherProps}
     >
@@ -74,14 +129,20 @@ function ShippingSimulation({
       </h2>
 
       <InputText
-        inputRef={postalCodeInputRef}
         actionable
+        error={errorMessage}
         id="shipping-postal-code"
         label="Zip Code"
+        value={shippingPostalCode}
+        onInput={handleOnInput}
         onSubmit={handleSubmit}
-        onInput={(e) => setPostalCodeInput(e.currentTarget.value)}
-        onClear={() => setPostalCodeInput('')}
-        value={postalCodeInput}
+        onClear={() => {
+          setShippingPostalCode('')
+          setPostalCodeLocation('')
+          setShippingOptions([])
+          setDisplayClearButton(false)
+        }}
+        displayClearButton={displayClearButton}
       />
 
       <Link href="/" data-fs-shipping-simulation-link>
@@ -89,37 +150,46 @@ function ShippingSimulation({
         <Icon name="ArrowSquareOut" width={18} height={18} />
       </Link>
 
-      <h3 data-fs-shipping-simulation-subtitle>Shipping options</h3>
-      <p className="text__body" data-fs-shipping-simulation-location>
-        {postalCodeLocation}
-      </p>
+      {hasShippingOptions && (
+        <>
+          <h3 data-fs-shipping-simulation-subtitle>Shipping options</h3>
+          <p className="text__body" data-fs-shipping-simulation-location>
+            {postalCodeLocation}
+          </p>
 
-      <Table data-fs-shipping-simulation-table>
-        <TableBody>
-          {shippingOptions.map((option) => (
-            <TableRow
-              key={option.carrier}
-              data-fs-shipping-simulation-table-row
-            >
-              <TableCell data-fs-shipping-simulation-table-cell>
-                {option.carrier}
-              </TableCell>
-              <TableCell data-fs-shipping-simulation-table-cell>
-                {option.estimate}
-              </TableCell>
-              <TableCell data-fs-shipping-simulation-table-cell>
-                <Price
-                  formatter={formatter}
-                  value={option.price}
-                  SRText="price"
-                />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          <Table data-fs-shipping-simulation-table>
+            <TableBody>
+              {shippingOptions.map((option) => (
+                <TableRow
+                  key={option.carrier}
+                  data-fs-shipping-simulation-table-row
+                >
+                  <TableCell data-fs-shipping-simulation-table-cell>
+                    {option.carrier}
+                  </TableCell>
+                  <TableCell data-fs-shipping-simulation-table-cell>
+                    {option.estimate}
+                  </TableCell>
+                  <TableCell data-fs-shipping-simulation-table-cell>
+                    <Price
+                      formatter={formatter}
+                      value={option.price}
+                      SRText="price"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </>
+      )}
     </section>
   )
+}
+
+// TODO Remove next lines after API integration
+ShippingSimulation.defaultProps = {
+  options: defaultShippingOptions,
 }
 
 export default ShippingSimulation
