7d52aac8a91f59d83ae62ca177e9fdd50f2b82b4
Filipe W. Lima <381395+filipewl@users.noreply.github.com>
2022-09-01 11:35:59 -0300

    Import the `SlideOver` from `@faststore/ui`

--- src/components/ui/SlideOver/SlideOver.tsx
+++ src/components/ui/SlideOver/SlideOver.tsx
@@ -1,57 +1,16 @@
-import { Modal as UIModal } from '@faststore/ui'
-import type { HTMLAttributes, ReactNode } from 'react'
+import { SlideOver as SlideOverUI } from '@faststore/ui'
+import type { SlideOverProps as SlideOverUIProps } from '@faststore/ui'
 
 import styles from './slide-over.module.scss'
 
-type Direction = 'leftSide' | 'rightSide'
-type WidthSize = 'full' | 'partial'
-
-interface SlideOverProps extends HTMLAttributes<HTMLDivElement> {
-  /**
-   * A boolean value that represents the state of the SlideOver
-   */
-  isOpen: boolean
-  /**
-   * Represents the side that the SlideOver comes from.
-   */
-  direction: Direction
-  /**
-   * Represents the size of the SlideOver.
-   */
-  size: WidthSize
-  children: ReactNode
-  /**
-   * Represents the fade effect of the SlideOver.
-   */
-  fade: 'in' | 'out'
-  /**
-   * This function is called whenever the user clicks outside
-   * the modal content
-   */
-  onDismiss?: () => void
-}
-
-function SlideOver({
-  isOpen,
-  direction = 'leftSide',
-  size = 'full',
-  fade = 'out',
-  children,
-  className,
-  ...otherProps
-}: SlideOverProps) {
+function SlideOver({ className, children, ...otherProps }: SlideOverUIProps) {
   return (
-    <UIModal
+    <SlideOverUI
       className={`${styles.fsSlideOver} ${className}`}
-      isOpen={isOpen}
-      data-fs-slide-over
-      data-fs-slide-over-direction={direction}
-      data-fs-slide-over-size={size}
-      data-fs-slide-over-state={fade}
       {...otherProps}
     >
       {children}
-    </UIModal>
+    </SlideOverUI>
   )
 }
 
