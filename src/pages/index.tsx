774e9cbb62350269a1754e554e17319de7560cc3
Victor Miranda <victor2142@gmail.com>
2022-10-11 16:10:18 -0300

    Trigger ci/cd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 function Page() {
-  return <>This is a placeholder PR to test CI/CD changes.</>
+  return <>This is a placeholder PR to test CI/CD changes. Testing again</>
 }
 
 export default Page
