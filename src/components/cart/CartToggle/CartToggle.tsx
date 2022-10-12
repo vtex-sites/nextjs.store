913bdb0e92a7e2610bf6d845e2469fdfc3215d3a
Lucas Feijó <lucas.portela@vtex.com.br>
2022-10-12 15:03:14 -0300

    chore: Minor `CartToggle` code refactor

--- src/components/cart/CartToggle/CartToggle.tsx
+++ src/components/cart/CartToggle/CartToggle.tsx
@@ -3,13 +3,13 @@ import Icon from 'src/components/ui/Icon'
 import { useCartToggleButton } from 'src/sdk/cart/useCartToggleButton'
 
 function CartToggle() {
-  const btnProps = useCartToggleButton()
+  const { 'data-items': totalItems, ...btnProps } = useCartToggleButton()
 
   return (
     <Button
       data-fs-button-cart
-      counter={btnProps['data-items']}
-      aria-label={`Cart with ${btnProps['data-items']} items`}
+      counter={totalItems}
+      aria-label={`Cart with ${totalItems} items`}
       icon={<Icon name="ShoppingCart" width={32} height={32} />}
       {...btnProps}
     />
