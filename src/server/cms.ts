7f6a0e6a86043618a55715aa19b463b8adfa64f8
Tiago Gimenes <tlgimenes@gmail.com>
2022-10-18 13:57:38 -0300

    remove requirement

--- src/server/cms.ts
+++ src/server/cms.ts
@@ -3,8 +3,6 @@ import type { Locator } from '@vtex/client-cms'
 
 import config from '../../store.config'
 
-const MIN_REQUIRED_SECTIONS = 3
-
 export const clientCMS = new ClientCMS({
   workspace: config.api.workspace,
   tenant: config.api.storeId,
@@ -19,13 +17,6 @@ export const getPageSections = async (options: Options) => {
       : await clientCMS
           .getCMSPagesByContentType(options)
           .then((pages) => pages.data[0])
-
-  // Requires minimum amount of sections per page so if one bugs, the page does not render a blank screen
-  if (page.sections.length < MIN_REQUIRED_SECTIONS) {
-    throw new Error(
-      `FastStore quality control requires at least ${MIN_REQUIRED_SECTIONS} sections per page. Please add more sections to page: "/"`
-    )
-  }
 
   return page.sections
 }
