3127f23de0aa36af8e4ac5d99c6434df6ec21c18
Tiago Gimenes <tlgimenes@gmail.com>
2022-10-18 10:40:19 -0300

    add const to varrs

--- src/server/cms.ts
+++ src/server/cms.ts
@@ -3,6 +3,8 @@ import type { Locator } from '@vtex/client-cms'
 
 import config from '../../store.config'
 
+const MIN_REQUIRED_SECTIONS = 3
+
 export const clientCMS = new ClientCMS({
   workspace: config.api.workspace,
   tenant: config.api.storeId,
@@ -19,9 +21,9 @@ export const getPageSections = async (options: Options) => {
           .then((pages) => pages.data[0])
 
   // Requires minimum amount of sections per page so if one bugs, the page does not render a blank screen
-  if (page.sections.length < 3) {
+  if (page.sections.length < MIN_REQUIRED_SECTIONS) {
     throw new Error(
-      `FastStore quality control requires at least 3 sections per page. Please add more sections to page: "/"`
+      `FastStore quality control requires at least ${MIN_REQUIRED_SECTIONS} sections per page. Please add more sections to page: "/"`
     )
   }
 
