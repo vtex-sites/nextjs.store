0a47cc1c0544f254c00656a5606d8384cec74008
Tiago Gimenes <tlgimenes@gmail.com>
2022-10-18 17:32:24 -0300

    apply requested changes

--- src/server/cms.ts
+++ src/server/cms.ts
@@ -20,14 +20,38 @@ const isLocator = (x: any): x is Locator =>
   typeof x.contentType === 'string' &&
   (typeof x.releaseId === 'string' || typeof x.documentId === 'string')
 
-export const getPage = <T extends ContentData>(options: Options) => {
-  const page = isLocator(options)
-    ? clientCMS.getCMSPage(options)
-    : clientCMS
-        .getCMSPagesByContentType(options.contentType, options.filters)
-        .then((pages) => pages.data[0])
+export const getPage = async <T extends ContentData>(options: Options) => {
+  const result = await (isLocator(options)
+    ? clientCMS.getCMSPage(options).then((page) => ({ data: [page] }))
+    : clientCMS.getCMSPagesByContentType(options.contentType, options.filters))
 
-  return page as Promise<T>
+  const pages = result.data
+
+  if (!pages[0]) {
+    throw new Error(
+      `Missing content on the CMS for content type ${
+        options.contentType
+      }. Add content before proceeding. Context: ${JSON.stringify(
+        options,
+        null,
+        2
+      )}`
+    )
+  }
+
+  if (pages.length !== 1) {
+    throw new Error(
+      `Multiple content defined on the CMS for content type ${
+        options.contentType
+      }. Remove duplicated content before proceeding. Context: ${JSON.stringify(
+        options,
+        null,
+        2
+      )}`
+    )
+  }
+
+  return pages[0] as T
 }
 
 export type PageContentType = ContentData & {
