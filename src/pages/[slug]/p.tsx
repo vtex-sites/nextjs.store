7669521c9393d9ded1510a933845790c6b05f647
Tiago Gimenes <tlgimenes@gmail.com>
2022-10-25 18:07:03 -0300

    use nextjs 13

--- src/pages/[slug]/p.tsx
+++ src/pages/[slug]/p.tsx
@@ -25,6 +25,7 @@ import storeConfig from '../../../store.config'
  * Sections: Components imported from '../components/sections' only.
  * Do not import or render components from any other folder in here.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const COMPONENTS: Record<string, ComponentType<any>> = {
   ProductDetails,
   BannerNewsletter,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,7 @@ import storeConfig from '../../store.config'
  * Sections: Components imported from '../components/sections' only.
  * Do not import or render components from any other folder in here.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const COMPONENTS: Record<string, ComponentType<any>> = {
   Hero,
   BannerText,
