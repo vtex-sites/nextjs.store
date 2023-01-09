2ea6843aa7b238fe1690c0cb5feeec6647ee3abb
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2023-01-09 11:03:15 -0300

    Add response type

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,5 +1,5 @@
-import type { NextApiRequest } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(_req: NextApiRequest, res) {
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(200).send('OK')
 }
