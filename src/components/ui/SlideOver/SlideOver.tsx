60a285d3c9027dbd695159063c5ede93d3d9fe8b
Arthur Andrade <arthurfelandrade@gmail.com>
2023-01-17 16:41:19 -0300

    feat: Update UIProvider imports
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/components/ui/SlideOver/SlideOver.tsx
+++ src/components/ui/SlideOver/SlideOver.tsx
@@ -44,6 +44,7 @@ function SlideOver({
     <UIModal
       className={`${styles.fsSlideOver} ${className}`}
       isOpen={isOpen}
+      data-modal={null}
       data-fs-slide-over
       data-fs-slide-over-direction={direction}
       data-fs-slide-over-size={size}
