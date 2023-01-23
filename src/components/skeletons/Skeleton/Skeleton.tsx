f88696454676beb3329ee7fe4e21285018cd43ff
Fanny <fanny.chien@vtex.com.br>
2023-01-23 19:28:39 +0800

    Chore: Attempts to use skeleton on productTiles

--- src/components/skeletons/Skeleton/Skeleton.tsx
+++ src/components/skeletons/Skeleton/Skeleton.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react'
-import { Skeleton as UISkeleton } from '@faststore/ui'
+// import { Skeleton as UISkeleton } from '@faststore/ui'
 
 import Shimmer from '../Shimmer'
 import styles from './skeleton.module.scss'
@@ -34,7 +34,7 @@ function Skeleton({
       className={styles.fsSkeleton}
       data-fs-skeleton-shimmer={shimmer}
     >
-      <UISkeleton
+      <div
         data-fs-skeleton
         data-fs-skeleton-variant={variant}
         {...otherProps}
